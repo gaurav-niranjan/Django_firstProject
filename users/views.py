dabba59c117e40e2059f54b92d5d65299ab5566b
gaurav <niranjangaurav8@gmail.com>
2021-10-24 00:21:21 +0530

    added basic auth

--- users/views.py
+++ users/views.py
@@ -1,9 +1,42 @@
 from django.contrib.admin import decorators
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
 from . models import Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 
+def loginUser(request):
+
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "Username does not exist")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)   #create a session for the user
+            return redirect('profiles')
+
+        else:
+            messages.error(request, "Username or password is incorrect")
+
+    return render(request, 'users/login_register.html')
+
+def logoutUser(request):
+    logout(request)
+    messages.error(request, "User was logged out")
+    return redirect('login')
 
 def profiles(request):
     profiles = Profile.objects.all()
